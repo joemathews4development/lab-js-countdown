27f9fcfc4bc23bf28c47ccadb67842c7cbbd603e
Ben Joe Mathews <joemathews4development@gmail.com>
2026-01-22 15:32:23 +0100

    Completed the 3 iterations and 2 bonus iterations of JS countdown

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+let buttonNode = document.querySelector("#start-btn")
+buttonNode.addEventListener("click", startCountdown)
+
+let divTimeNode = document.querySelector("#time")
+let toastCardNode = document.querySelector("#toast")
+let toastCardMeassageNode = document.querySelector("#toast-message")
+let toastCloseButtonNode = document.querySelector("#close-toast")
 
 
 
@@ -14,9 +21,19 @@ let timer = null; // Variable to store the interval
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
-
-
-  // Your code goes here ...
+  let countdownIntervalId = setInterval(() => {
+    if (remainingTime === DURATION) {
+      showToast("⏰ Final countdown! ⏰")
+    }
+    remainingTime -= 1
+    divTimeNode.innerText = remainingTime
+    if (remainingTime === 0) {
+      clearInterval(countdownIntervalId)
+      showToast("Lift off! 🚀")
+    } else if (remainingTime === 5) {
+      showToast("Start the engines! 💥")
+    }
+  }, 1000)
 }
 
 
@@ -25,10 +42,21 @@ function startCountdown() {
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
+  toastCardMeassageNode.innerText = message
+  toastCardNode.classList.add("show")
 
-  // Your code goes here ...
+  let timeOutId = setTimeout(() => {
+    toastCardNode.classList.remove("show")
+    if (remainingTime === 0) {
+      remainingTime = DURATION
+      divTimeNode.innerText = remainingTime
+    }
+  }, 3000);
 
-
+  toastCloseButtonNode.addEventListener(("click"), () => {
+    toastCardNode.classList.remove("show")
+    clearTimeout(timeOutId)
+  })
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
